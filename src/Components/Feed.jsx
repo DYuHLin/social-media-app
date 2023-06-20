14b46f5e95f8c10ce7d460258f60c5c66c04e795
DYuHLin <damianver2000@gmail.com>
2023-06-20 11:10:20 +0200

    Finally finished the likes system

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,4 +1,4 @@
-import { arrayRemove, arrayUnion, collection, doc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { arrayRemove, arrayUnion, collection, doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
 import React, { useContext, useEffect, useState } from 'react'
 import { db } from '../Firebase';
 import UserContext from '../Context/UserContext';
@@ -8,41 +8,84 @@ const Feed = () => {
     const colRef = collection(db, 'posts');
     const {currentUser} = useContext(UserContext);
 
-    const like = (id) => {
-        onSnapshot(doc(db, "posts", id), async (docs) => {
-            console.log(docs.data().likes);
-            if(!docs.data().likes.useId){
-            await updateDoc(doc(db, 'posts', id), {
-                 likes: arrayUnion({
-                     useId: currentUser.uid,
-                     displayName: currentUser.displayName,
-                     likes: "yes"
-                 })
-            });
-            } else if (docs.data().likes.useId && docs.data().likes.likes === "no") {
-                console.log("here")
+    const like = async (id, userId) => {       
+        try{
+            const docu = await getDoc(doc(db, "posts", id));
+            console.log(docu.data().likes);
+            let obj = docu.data().likes.find((x) => x.useId === userId);
+            if(!docu.data().likes.find((x) => x.useId === userId)){
+                return updateDoc(doc(db, 'posts', id), {
+                    likes: arrayUnion({
+                        useId: currentUser.uid,
+                        displayName: currentUser.displayName,
+                        likes: "yes"
+                    }),
+                    likeCount: docu.data().likeCount +1
+                });
+            } else if(obj && obj.likes === "no"){
                 await updateDoc(doc(db, 'posts', id), {
-                    likes: arrayRemove(docs)
-               });
-            }
-         });   
+                    likes: arrayRemove(obj)
+                });
+
+                 await updateDoc(doc(db, 'posts', id), {
+                    likes: arrayUnion({
+                        useId: currentUser.uid,
+                        displayName: currentUser.displayName,
+                        likes: "yes"
+                    }),
+                    likeCount: docu.data().likeCount +2
+                });
+            }else{               
+                return updateDoc(doc(db, 'posts', id), {
+                    likes: arrayRemove(obj),
+                    likeCount: docu.data().likeCount -1
+                });
+            }; 
+             
+           } catch(err){
+             console.log(err);
+           }; 
     };
 
-    const dislike = (id) => {
-         onSnapshot(doc(db, "posts", id), async (docs) => {
-            console.log(docs.data().likes);
-            if(!docs.data().likes.useId){
-            await updateDoc(doc(db, 'posts', id), {
-                 likes: arrayUnion({
-                     useId: currentUser.uid,
-                     displayName: currentUser.displayName,
-                     likes: "no"
-                 })
-            });
-            } else if (docs.data().likes.useId) {
-                console.log("here")
-            }
-         });           
+    const dislike = async (id, userId) => {
+        try{
+            const docu = await getDoc(doc(db, "posts", id));
+            console.log(docu.data().likes);
+            let obj = docu.data().likes.find((x) => x.useId === userId);
+
+            if(!docu.data().likes.find((x) => x.useId === userId)){
+                return updateDoc(doc(db, 'posts', id), {
+                    likes: arrayUnion({
+                        useId: currentUser.uid,
+                        displayName: currentUser.displayName,
+                        likes: "no"
+                    }),
+                    likeCount: docu.data().likeCount -1
+                });
+            } else if(obj && obj.likes === "yes"){
+                await updateDoc(doc(db, 'posts', id), {
+                    likes: arrayRemove(obj),
+                    likeCount: docu.data().likeCount -1
+                });
+
+                await updateDoc(doc(db, 'posts', id), {
+                    likes: arrayUnion({
+                        useId: currentUser.uid,
+                        displayName: currentUser.displayName,
+                        likes: "no"
+                    }),
+                    likeCount: docu.data().likeCount -2
+                });
+            }else{                
+                return updateDoc(doc(db, 'posts', id), {
+                    likes: arrayRemove(obj),
+                    likeCount: docu.data().likeCount +1
+                });
+            }; 
+             
+           } catch(err){
+             console.log(err);
+           };           
     };
 
     useEffect(() => {
@@ -62,9 +105,9 @@ const Feed = () => {
                 <div className='post' key={obj.idPost}>
                     <div className="postContainer">
                         <div className="vote">
-                            <i onClick={() => like(obj.idPost)} className='bx bx-up-arrow-alt like'></i>
-                            <span>12</span>
-                            <i onClick={() => dislike(obj.idPost)} className='bx bx-down-arrow-alt' ></i>   
+                            <i onClick={() => like(obj.idPost, currentUser.uid)} className='bx bx-up-arrow-alt'></i>
+                            <span>{obj.likeCount}</span>
+                            <i onClick={() => dislike(obj.idPost, currentUser.uid)} className='bx bx-down-arrow-alt' ></i>   
                         </div>
                         <div className="postContent">
                             <div className="postInfo">
